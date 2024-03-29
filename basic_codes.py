86919966631b179309aaff1ab3addf211b02691a
vyankatesh pandit <ompandit1511@gmail.com>
2024-03-30 00:15:39 +0530

    new codes added in basic codes file

--- basic_codes.py
+++ basic_codes.py
@@ -91,8 +91,93 @@
 
 
 
+
 # ---------------------------------------------------------------------
 
+# Loops
 
- 
+
+# name = input("Enter Your Name :")
+
+# value = int(input("Enter How Many Times You Want To Print Your Name :"))
+# i=1
+# while i <= value :
+#     print(i,")",name)
+#     i=i+1
+
+
+# examples
+
+
+# 1) 1 to 100
+# num = int(input("Enter Number :"))
+# i = 1
+# while i <= num :
+#     print(i)
+#     i=i+1
+
+# 2) 100 1 to 
+# num = int(input("Enter Number :"))
+# i = num
+# while i >= 1 :
+#     print(i)
+#     i=i-1
+
+
+# 3) multiplication Table
+
+# num = int(input("Enter Number :"))
+# i = 1
+# while i <= 10 :
+#       print(num,"X",i,"=",num*i) 
+#       i = i + 1
+
+
+# 4) Print elements of list 
+
+
+# nums = [1,2,4,6,8,9,5]
+
+# idx = 0
+# while idx < len(nums):
+#     print(nums[idx])
+#     idx = idx +1
+
+
+# 5) search number in tuple
+
+
+
+# list = (1,2,3,4,5,6,7)
+# i = 0
+# x = int(input("Enter Number :"))
+# while i < len(list):
+#     if(list[i] == x):
+#       print("Found At Index",i)
+#       break
+#     i = i+1
+
+
+
+# break and continue keyword
+
+# i = 0
+# while i<=10:
+#     if (i == 3):
+#      i=i+1
+#      continue
+#     print(i)
+#     i +=1 
+
+
+
+
+
+
+
+
+# ---------------------------------------------------------------------
+
+# list and tuple 
+
 
